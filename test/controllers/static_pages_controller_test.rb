55b9d483bbdd1249e1afe2286625c0bd7c57185e
phuongthaonguyen2912 <phuongthaonguyen2912@gmail.com>
2016-10-04 00:10:16 +0700

    chapter 8

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,21 +2,21 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title" , "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
-    assert_select "title" , "Ruby on Rails Tutorial Sample App| Help"
+    assert_select "title" , "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
-    assert_select "title" , "Ruby on Rails Tutorial Sample App| About Us"
+    assert_select "title" , "About Us | Ruby on Rails Tutorial Sample App"
   end
 
 end
