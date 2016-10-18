c52f5d6f786a3a37277915a03e5fbbdb619b6f80
phuongthaonguyen2912 <phuongthaonguyen2912@gmail.com>
2016-10-18 10:24:33 +0700

    User microposts

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
 
   resources :account_activations, only: :edit
 
-  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :password_resets, except: [:index, :show, :destroy]
+
+  resources :microposts, only: [:create, :destroy]
 
 end
